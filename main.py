884cf865faddb1359066b239245fe5615aa8c8c8
SimRCA <73945868+SimRCA@users.noreply.github.com>
2023-11-25 18:45:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import config as cg
 from RulesClassification import RulesClassification
 from DecisionTreeClassification import DecisionTreeClassification
+from RandomForestClassifier import RandomForestClassifier
 data = pd.read_csv('data/abnormal_kpi_file.csv')
 kpi_title = ["down_MAC_throughput(bps)",
              "up_MAC_throughput(bps)",  "RSRP(dBm)", "RSRQ(dB)"]
@@ -10,4 +11,5 @@ x = data[kpi_title]
 y = data.iloc[:,-1]
 RulesClassification(x, y)
 DecisionTreeClassification(x,y)
+RandomForestClassifier(x,y)
 
